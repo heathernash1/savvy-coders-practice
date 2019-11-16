c570e257665a2316c27a6b8f8d51c0cf394b48c5
heathernash1 <nash.heatherm@gmail.com>
2019-11-15 21:33:06 -0600

    random students app get M Names

--- app.js
+++ app.js
@@ -4,9 +4,21 @@
 const students = [
 "Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"];
 
+function getMs(namesArr){
+  const mNames = [];
+  for (let i =0; i < namesArr.length; i += 1){
+    if (namesArr[i].startsWith("M")){
+      mNames.push(namesArr[i]);
+    }
+  }
+  return mNames;
+}
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 console.log(students[getRandomInt(0, students.length-1)]);
+
+console.log(getMs(students));
