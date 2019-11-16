4a79fd49f9b0a78aa164ab751901fca489da80ad
heathernash1 <nash.heatherm@gmail.com>
2019-11-15 21:05:23 -0600

    create array of student names

---
+++ app.js
@@ -0,0 +1,7 @@
+// arrays are objects [] objects have named keys and auto incremented numerical keys A.K.A index
+// collections are composite data types
+const myArr = [
+"Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"];
+
+
+
