8b7911648fd01e1e27464357b535ee9015b63fc4
heathernash1 <nash.heatherm@gmail.com>
2019-11-15 21:19:20 -0600

    random students app

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-// arrays are objects [] objects have named keys and auto incremented numerical keys A.K.A index
+// arrays are objects [] objects have named keys
+//and auto incremented numerical keys A.K.A index
 // collections are composite data types
-const myArr = [
+const students = [
 "Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"];
 
-
-
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+console.log(students[getRandomInt(0, students.length-1)]);
