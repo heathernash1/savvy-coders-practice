4637cc7592fe3c57cb3e718e86db4ae45dbc7273
heathernash1 <nash.heatherm@gmail.com>
2019-11-19 16:21:06 -0600

    update to shows function

--- constructor.js
+++ constructor.js
@@ -26,3 +26,40 @@ console.log(me);
 console.log(me.canDrink());
 console.log(me.getFullName());
 console.log(me.getBio());
+
+function shows( tvShow, network, numShows, fan) {
+  this.tvShow = tvShow;
+  this.network = network;
+  this.numShows = numShows;
+  this.fan = fan;
+  this.getfavoriteShows = function (){
+     return ` Supergirl, Expanse, Supernatural, and Doctor Who are some of my favorite shows. `
+   }
+
+
+  this.getsuperFan = function() {
+    if (this.numShows > 2)
+      return `If you like to watch science fiction shows. We should definitely have a watch party sometime!`
+      else
+      return `I like to watch dramas, too.`
+    }
+
+
+this.getGeek = function() {
+      return `Hi!${this.getfavoriteShows()} If you watch these, then you are definitely a ${this.fan} fan . `
+       }
+  ;
+
+this.getShowInfo = function() {
+      return `${this.tvShow} on ${this.network} is a new favorite.`
+    }
+    ;
+
+}
+//function constructor is keyword new
+//instantiate we create variables
+const watch = new shows ('Emergence','ABC', 1, 'SyFy');
+console.log(watch);
+console.log(watch.getGeek());
+console.log(watch.getShowInfo());
+console.log(watch.getsuperFan());
