3ee9f7ba71fe4ab29492d6311894b130bcc827f4
heathernash1 <nash.heatherm@gmail.com>
2019-11-25 21:28:24 -0600

    last update

--- homework7.js
+++ homework7.js
@@ -8,17 +8,19 @@ class Shows {
   }
 
 
+
   //these are functions to use on the "New Objects"
     get favoriteShows (){
       return ` Supergirl, Expanse, Supernatural, and Doctor Who are some of my favorite shows. `;
      }
 
-
+set favoriteShows (favoriteShows) {
+  this.fav = favoriteShows;
+}
     get superFan () {
      if (this.numShows > 2) {
         return `If you like to watch science fiction shows. We should definitely have a watch party sometime!`;
-     }
-        else {
+     } else {
        return `I like to watch dramas, too.`;
       }
     }
@@ -33,8 +35,22 @@ class Shows {
         return `${this.tvShow} on ${this.network} is a new favorite.`;
       }
     }
+
       //setter
+
+
 
    const redo = new Shows ('Xfiles','FOX', 5, "Mystery");
 
-  console.log(redo)
+
+  // console.log(redo);
+
+  class addMovie extends Shows{
+    constructor (tvShow, network, numShows, fan, movie ){
+      super( tvShow, network, numShows, fan);
+      this.film = movie;
+    }
+  }
+const picture = new addMovie("Dancing with the stars", "ABC", 3,"Comedy", "Coming to America");
+// console.log(picture);
+console.log(favoriteShows);
